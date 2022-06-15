71d852b239f9c24744d8d069820c5eb226e4cf42
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-06-15 17:50:38 +0100

    html css done

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,15 +3,14 @@
 const themeButton = document.querySelector('.theme-btn');
 const themeButtonImage = document.querySelector('.theme-icon');
 
-
 themeButton.addEventListener('click', function(){
     
     if (document.documentElement.dataset.theme === 'dark') {
         document.documentElement.dataset.theme = 'light';  
-        themeButtonImage.src = 'assets/images/icon-sun.svg';
+        themeButtonImage.src = 'assets/images/icon-moon.svg';
     }
     else {
         document.documentElement.dataset.theme = 'dark';  
-        themeButtonImage.src = 'assets/images/icon-moon.svg';
+        themeButtonImage.src = 'assets/images/icon-sun.svg';
     }
 }); 
