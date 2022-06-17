58a566f497b8e67af1fb8629a08a0cab98f75253
guseinguseinov <gusein.guseinov2002@gmail.com>
2022-06-17 13:48:24 +0100

    list add done, checkbox fixed, delete done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,15 @@
 'use strict';
 
-const themeButton = document.querySelector('.theme-btn');
-const themeButtonImage = document.querySelector('.theme-icon');
-const userForm = document.querySelector('.user-form');
-const userList = document.querySelector('.list');
-const checkBoxList = document.querySelectorAll('.list-item-checkbox');
+let themeButton = document.querySelector('.theme-btn');
+let themeButtonImage = document.querySelector('.theme-icon');
+let userForm = document.querySelector('.user-form');
+let userList = document.querySelector('.list');
+let allList = [];
+let activeList = [];
+let completedList = [];
 
-
-checkBoxList.forEach( function(element) {
-    element.addEventListener('click', function(e){
-        console.log(e);
-    })    
-})
-
-
+// dark mode handler 
 themeButton.addEventListener('click', function(){
-    
     if (document.documentElement.dataset.theme === 'dark') {
         document.documentElement.dataset.theme = 'light';  
         themeButtonImage.src = 'assets/images/icon-moon.svg';
@@ -27,39 +21,48 @@ themeButton.addEventListener('click', function(){
 }); 
 
 
-// document.addEventListener('keyup', function(e) {
-//     userForm.preventDefault();
-//     if (e.key === 'Enter'){
-//         console.log('ok');
-//     }
-// });
+userForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let {checkbox, text} = userForm.elements;
+    if (text.value.length === 0) {
+        alert('Please, enter something.');
+    }
+    else {
+        let newItem = `
+        <div class="list-item">
+            <div class="list-container">
+                <input class="list-item-checkbox" type="checkbox" >
+                <p class="text">${text.value}</p>
+            </div>
 
-function check(checkInput, text) {
+            <button class="delete" type="button">
+                <img src="./assets/images/icon-cross.svg" aria-label="delete">
+            </button>
+        </div>`
+
+        userList.insertAdjacentHTML('afterbegin', newItem);
+        text.value = '';
+        document.querySelector('.list-item-checkbox').addEventListener('click', checkCompleted); 
+        document.querySelector('.delete').addEventListener('click', deleteITem);
+        if (checkbox.checked) {
+            document.querySelector('.list-item-checkbox').checked = true;
+            document.querySelector('.text').classList.add('line-through');
+        }
+    }
     
+});
+
+function checkCompleted() {
+    let text = this.parentElement.children[1];
+    if (this.checked) {
+        text.classList.add('line-through');
+    }else {
+        text.classList.remove('line-through');
+    }
+}
+
+function deleteITem() {
+    this.parentElement.remove();
 }
 
 
-
-
-userForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    const  [ischecked, listItem]= userForm.elements;
-    let newItem = `
-    <div class="list-item">
-        <div class="list-container">
-            <input class="list-item-checkbox" type="checkbox" >
-            <p class="text">${listItem.value}</p>
-        </div>
-
-        <button class="delete" type="button">
-            <img src="./assets/images/icon-cross.svg" aria-label="delete">
-        </button>
-    </div>`
-
-    userList.insertAdjacentHTML('afterbegin', newItem);
-    [checkBoxList].push(ischecked);
-    if (ischecked.checked == true) {
-        document.querySelector('.list-item-checkbox').checked = true;
-        document.querySelector('.text').classList.add('line-through');
-    }
-});
